cd94bc9efd98a8a3649d54a7a3146341296f677e
huro3h_satoshi <huro3h@gmail.com>
2016-04-26 07:54:40 +0800

    init commit

---
+++ myCalc/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  myCalc
+//
+//  Created by satoshiii on 2016/04/26.
+//  Copyright © 2016年 satoshiii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var result: UILabel!
+
+
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
