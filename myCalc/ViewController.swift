930060a8d312d3ef2d76a12e562e1183ca084b00
huro3h_satoshi <huro3h@gmail.com>
2016-04-26 16:23:40 +0800

    一旦全ての関連付け解除

--- myCalc/ViewController.swift
+++ myCalc/ViewController.swift
@@ -10,21 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var result: UILabel!
-
+    @IBOutlet weak var btn7: UIButton!
 
 
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
